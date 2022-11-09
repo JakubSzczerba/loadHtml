34cbfe371dfbb0d6107697c6d970b7fd9ae5cdff
JakubSzczerba <jakszczerb@gmail.com>
2022-11-09 19:12:25 +0100

    Add web server bundle.

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
